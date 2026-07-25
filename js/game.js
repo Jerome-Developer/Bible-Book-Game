8707d7a6aab2eed7d594141f0aa4402aa12f04e5
Jerome-Developer <jtcrtcpj4@gmail.com>
2026-07-24 21:23:31 -0500

    Update game.js
    Added code for swapping 2 books and function for calculating results

--- js/game.js
+++ js/game.js
@@ -14,15 +14,16 @@ function Create_Elements(Array2)
     const Container = document.getElementById("book-container");
     Array2.forEach(element => {
         const div = document.createElement("div");
-        /* div.className = "Class Name"; */
+        /* div.className = "bible-book"; */
         div.textContent = element;
-        div.addEventListener("onclick", );
+        div.addEventListener("click", Book_Clicked);
         Container.appendChild(div);
     });
 }
 
-function Book_Clicked(caller)
+function Book_Clicked(event)
 {
+    let caller = event.currentTarget;
     if (caller.classList.contains("selected"))
     {
         caller.classList.toggle("selected");
@@ -30,13 +31,48 @@ function Book_Clicked(caller)
 
     else
     {
-        if(document.querySelectorAll("selected").length = 1)
+        if(document.querySelectorAll(".selected").length == 1)
         {
-            /* Swap Books */
+            let Second_Book = document.querySelectorAll(".selected")[0];
+            [caller.textContent, Second_Book.textContent] = [Second_Book.textContent, caller.textContent];
+            Second_Book.classList.toggle("selected");
+
+            caller.classList.remove("correct");
+            caller.classList.remove("incorrect");
+            Second_Book.classList.remove("correct");
+            Second_Book.classList.remove("incorrect");
         }
         else
         {
             caller.classList.toggle("selected");
         }
     }
+}
+
+function Get_Results()
+{
+    /* hide/lock book section */
+    let Answers = document.querySelectorAll("#book-container > div");
+    let Total_Correct = 0;
+    const TOTAL_BOOKS = 66;
+
+    let Book_Tracker = 0;
+
+    Answers.forEach(Answer_Book => {
+        if(Answer_Book.textContent == BIBLE_BOOKS[Book_Tracker])
+        {
+            Total_Correct++;
+            Answer_Book.classList.remove("incorrect");
+            Answer_Book.classList.add("correct");
+        }
+        else
+        {
+            Answer_Book.classList.remove("correct");
+            Answer_Book.classList.add("incorrect");
+        }
+        Book_Tracker++;
+    });
+
+    document.getElementById("score").textContent = "Score: " + Total_Correct + " / " + TOTAL_BOOKS;
+    document.getElementById("percent-score").textContent = "Percent Score: " + ((Total_Correct / TOTAL_BOOKS) * 100).toFixed(2) + "%";
 }
