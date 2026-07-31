4bb174f5b3eccc9b70a38f3da8cfabca10963bcb
Jerome-Developer <jtcrtcpj4@gmail.com>
2026-07-30 21:44:33 -0500

    Created a working start button with start menu
    The start menu now shows when the page loads instead of the game board section which now displays after clicking the start button

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
-window.addEventListener("load", Setup_Game);
+document.getElementById("start-button").addEventListener("click", Setup_Game);
 document.getElementById("submit").addEventListener("click", Get_Results);
 
 function Setup_Game()
 {
+    Hide_Start_Menu();
     let Shuffled_Books = Shuffle_Array(BIBLE_BOOKS.slice());
     Create_Elements(Shuffled_Books);
+}
+
+function Hide_Start_Menu()
+{
+    document.getElementById("start-menu").style.display = "none";
 }
